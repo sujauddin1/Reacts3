4b8d135e5aca2b27c0673feaf4a7b659afd90479
sujauddin <sujauddinkazi282538@gmail.com>
2023-02-04 23:19:53 +0530

    react3 assingnment

---
+++ src/Component/Card.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+ export const Card=()=>{
+    return(
+        <div>
+        <h1> i am card</h1>;
+        <img src="https://images.ctfassets.net/yr4qj72ki4ky/legacyBlogPost77Thumbnail/cd4783ad7b35efc4367166a570a9952e/bigstock-Real-Java-Script-Code-Developi-217215433.jpg" alt="" />
+        </div>
+    )
+}
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export const Navbar=()=>{
+    return(
+        <div>
+            <h1>i am navigation </h1>
+        </div>
+    )
+};
